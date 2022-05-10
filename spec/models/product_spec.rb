465ddcea920d17361c7676921d3919bb8e40ca45
Grace <wangxiaotian2012@gmail.com>
2022-05-10 11:00:26 -0700

    Add 5 tests for the product model validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,5 +3,40 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
+    before do
+      @category = Category.create(name: "Cacti")
+    end
+
+    it "creates a product with all four fields set" do
+      @product = @category.products.create!(name: "Prickly Pear", price: 2500, quantity: 38, category: @category)
+
+      expect(@product).to be_valid
+    end
+
+    it "raise an exception for the name attribute if the product is saved without a name" do
+      @product = @category.products.create(name: nil, price: 2500, quantity: 38, category: @category)
+
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "raise an exception for the price attribute if the product is saved without a price" do
+      @product = @category.products.create(name: "Prickly Pear", quantity: 38, category: @category)
+
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "raise an exception for the quantity attribute if the product is saved without a quantity" do
+      @product = @category.products.create(name: "Prickly Pear", price: 2500, quantity: nil, category: @category)
+
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "raise an exception for the category attribute if the product is saved without a category" do
+      @product = Product.create(name: "Prickly Pear", price: 2500, quantity: 38)
+
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
   end
+
 end
